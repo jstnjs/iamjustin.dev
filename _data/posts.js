d7a3aa992d38f9a86077ad8abe1d5e8dbf648115
Visserman <AO617529@achmea.nl>
2022-07-01 16:42:20 +0200

    Working on styling

--- _data/posts.js
+++ _data/posts.js
@@ -1,23 +1,48 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
-module.exports = async function() {
-	try {
-    let url = "https://dev.to/api/articles/me/unpublished";
+// module.exports = async function() {
+// 	try {
+//     let url = "https://dev.to/api/articles/me/unpublished";
 
-		let posts = await EleventyFetch(url, {
-			duration: "1d",
-			type: "json",
-            fetchOptions: {
-                headers: {
-                    "api-key": process.env.DEVTO_API_KEY
-                }
-            }
-		});
+// 		let posts = await EleventyFetch(url, {
+// 			duration: "1d",
+// 			type: "json",
+//             fetchOptions: {
+//                 headers: {
+//                     "api-key": process.env.DEVTO_API_KEY
+//                 }
+//             }
+// 		});
 
-        return posts;
-	} catch(e) {
-        console.log('Failed to fetch articles. Return empty array.');
+//         return posts;
+// 	} catch(e) {
+//         console.log('Failed to fetch articles. Return empty array.');
 
-		return [];
-	}
-};
+// 		return [];
+// 	}
+// };
+
+module.exports = function() {
+	return [
+		{
+			title: "A blog to test",
+			description: 'Pretty short descriptiion',
+			slug: "post-1"
+		},
+		{
+			title: "A blog to test",
+			description: 'Pretty short descriptiion. This is a longer description that eventually need to be a excerpt. For now Ill use it like this..',
+			slug: "post-2"
+		},
+		{
+			title: "A blog to test",
+			description: 'Pretty short descriptiion',
+			slug: "post-3"
+		},
+		{
+			title: "A blog to test",
+			description: 'Pretty short descriptiion',
+			slug: "post-4"
+		}
+	]
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '_includes/layouts/*.njk'
+    '_includes/**/*.njk'
   ],
   theme: {
     extend: {},
